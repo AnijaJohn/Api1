8eba2666c7e5c557d0ac6f74b540abfd7403b732
Anija K John <anijakjohn1@gmail.com>
2021-11-10 17:30:37 +0530

    Webclient setup

--- src/main/java/api1/controller/UserController.java
+++ src/main/java/api1/controller/UserController.java
@@ -5,24 +5,49 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import api1.model.Api1Response;
 import api1.model.Users;
 import api1.service.UserService;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api1")
 public class UserController {
+
 	
 	@Autowired
-	UserService service;
+	UserService userService;
 	
-	@GetMapping("/users")
-	public ResponseEntity<List<Users>> getAllUsers()
-	{
-		return new ResponseEntity<List<Users>>(service.getUsers(),HttpStatus.OK);
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Api1Response> getUserById(@PathVariable Long id) {
+	return new ResponseEntity<Api1Response>(userService. getSingleUser(id), HttpStatus.OK);
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Api1Response> addUser(@RequestBody Users user) {
+	return new ResponseEntity<Api1Response>(userService.saveUser(user), HttpStatus.CREATED);
+	}
+	
+	
+	@PutMapping("/update")
+	public ResponseEntity<Api1Response> updateUser(@RequestBody  Users user) {
+	return new ResponseEntity<Api1Response>(userService.updateUser(user), HttpStatus.OK);
+	}
+	
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Api1Response> deleteUser(@PathVariable("id") Long id){
+	return new ResponseEntity<Api1Response>(userService.deleteUser(id), HttpStatus.OK);
+	}
 	}
 
-}
--- src/main/java/api1/service/UserService.java
+++ src/main/java/api1/service/UserService.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import api1.model.Api1Response;
 import api1.model.Users;
+import reactor.core.publisher.Mono;
 
 
 public interface UserService {
 	
-	List<Users> getUsers();
+	//List<Users> getUsers();
 
 	Api1Response saveUser(Users user);
 	
 	Api1Response getSingleUser (Long id);
+	
+	//List<Users> getSingleUser (Long id);
 	
 	Api1Response deleteUser(Long id);
 	
